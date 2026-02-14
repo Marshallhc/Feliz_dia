9428423431a5fcc9ee4310a35d66e6d2c853536f
Marshallhc <marshallcamara777@gmail.com>
2026-02-14 11:04:55 -0300

    Refactor Index component structure and imports

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -28,7 +28,7 @@ const Index = () => {
           imageAlt="que bella que esss"
           caption="— Recorrido"
           title="Perdón y Gracias"
-          body="Buenos y malos recuerdos.. gracias por ser vos en cada uno. Altos y bajos, gracias por estar en cada uno. Este es un regalito para vos, aún no soy tu novio pero estoy super enamorado de vos. Charlas, salidas, risas, enojos, encuentros, tiempos, miradas, detalles, no, sí, abrazos, caminatas, atención, todo y más, fué hecho con amor, porque sin esperar algo a cambio me diste mucho más de lo que crees, y no suelo pensar mucho en una persona pero estás en mi mente todos los días mujer, que el Señor siempre te mande un abrazo de mi parte, y el beso me lo guardo jajaja, te amo mujer, bastante, extraño siempre esos abrazos espontáneos..."
+          body="Buenos y malos recuerdos.. gracias por ser vos en cada uno. Altos y bajos, gracias por estar en cada uno. Este es un regalito de una de las cosas que mejor hago para vos. Charlas, salidas, risas, enojos, encuentros, tiempos, miradas, detalles, no, sí, abrazos, caminatas, atención, todo y más, fué hecho con amor, porque sin esperar algo a cambio me diste mucho más de lo que crees, y no suelo pensar mucho en una persona pero estás en mi mente todos los días mujer, que el Señor siempre te mande un abrazo de mi parte, y el beso me lo guardo jajaja, te amo mujer, bastante, extraño siempre esos abrazos espontáneos..."
           reverse
         />
 
@@ -43,4 +43,5 @@ const Index = () => {
 };
 
 export default Index;
+
 
